1ac0e820e1c7edcc96ce8c330aa2780c667c1f50
gashawgedef@gmail.com <gashawgedef@gmail.com>
2024-06-08 23:04:31 +0300

    user defined modules

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+from square import Square
+from triangle import Triangle
+
+
+s1=Triangle()
+s1.set_value(10,10)
+print(s1.area())
+
+
+s=Square()
+s.set_value(12,13)
+print(s.area())
+
---
+++ triangle.py
@@ -0,0 +1,7 @@
+
+from polygon import Polygon
+
+
+class Triangle(Polygon):
+    def area(self):
+        return self.get_width()*self.get_height()*1/2
