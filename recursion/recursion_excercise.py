30f6aaea880c25f94643db51ee32f86962590f28
gashawgedef@gmail.com <gashawgedef@gmail.com>
2024-06-24 23:35:43 +0300

    recursion

---
+++ recursion/recursion_excercise.py
@@ -0,0 +1,26 @@
+# def recursion():
+#     print("Hello")
+#     recursion()
+
+# recursion()
+# def fun():
+#     val = 10
+#     return val
+# def example():
+#     print("UL")
+#     x = fun()
+#     y =20
+#     result = x+y
+#     return result
+
+# print(example())
+def run(n):
+    if n == 0:
+        return
+    print(n)
+    run(n-1)
+    print("Ul")
+
+n = 3
+run(n)
+print("Completed")
