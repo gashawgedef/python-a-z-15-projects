438ace54fdbb1ec49ea243d4d82c0e5d8e11e25f
gashawgedef@gmail.com <gashawgedef@gmail.com>
2024-06-26 04:32:12 +0300

    datetime module

---
+++ datetime_module/scripts.py
@@ -0,0 +1,12 @@
+import datetime
+# dt =datetime.datetime.now()
+# date_today =datetime.date.today()
+# dt1 = datetime.date(2024,6,26)
+# dt2 = datetime.timedelta(40)
+# print(dt1+dt2)
+# print(date_today)
+dt1 = datetime.date.today()
+print("Current Year:", dt1.year)
+print("Current Month:", dt1.month)
+print("Current Day:", dt1.day)
+print(dt1.strftime("%A ,%B %d, %Y"))
